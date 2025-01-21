d49c94a852f80ee145a7766d28f2a0f93c8b5286
hdy510 <157979173+hdy510@users.noreply.github.com>
2025-01-21 16:08:02 +0900

    오토플레이

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const mediaQueryMobile = window.matchMedia("(max-width: 640px)");
 // 전체 페이지 슬라이드 Swiper 설정
 const swiper = new Swiper('.view.swiper', {
   direction: 'vertical',
-  // autoplay: {
-  //   delay: 4000,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
   mousewheel: {
     enabled: false, // 초기에는 Swiper 스크롤 비활성화
   },
